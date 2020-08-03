2782f6f4e4247ebd1bb410d92d3b03b81cfb45c5
Jose Guaura <jguaura@gmail.com>
2020-08-03 11:33:29 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    setMessages([...messages, input])
+  }
+
   return (
     <div className="app">
       <h1>Messenger GO BRRR 🔥 </h1>
-      <input type="text"/>
-      <button>send</button>
+      <input value={input} onChange={e => setInput(e.target.value)} type="text"/>
+      <button onClick={sendMessage}>send</button>
     </div>
   );
 }
