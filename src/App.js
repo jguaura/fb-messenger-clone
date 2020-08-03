29e0193a4cef18874589bfbf1636bc920f27ea5f
Jose Guaura <jguaura@gmail.com>
2020-08-03 18:48:55 -0300

    added firebase and styles in compnents

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
+import Message from './components/Message/Message';
 
 function App() {
 
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState('');
+  const [messages, setMessages] = useState([{username: 'Jos', message:'msg44'}, {username: 'Jos', message:'msg22'}, {username: 'Jos', message: 'mshg1'}])
 
   const sendMessage = (e) => {
     e.preventDefault()
-    setMessages([...messages, input])
+    input && setMessages([...messages, {username: username, message:input}])
+    setInput('');
   }
+
+  const check = (e) => {
+    if(e.key == 'Enter' && input) {
+      sendMessage(e)
+    }
+  }
+
+  useEffect(() => {
+    setUsername(prompt('What is your name?'))
+  }, [])
 
   return (
     <div className="app">
       <h1>Messenger GO BRRR 🔥 </h1>
-      <input value={input} onChange={e => setInput(e.target.value)} type="text"/>
-      <button onClick={sendMessage}>send</button>
+      <h2>Hi {username}</h2>
+        <FormControl onKeyPress={e => check(e)}>
+          <InputLabel>Enter a Message</InputLabel>
+          <Input value={input} onChange={e => setInput(e.target.value)}/>
+          <Button
+          disabled={!input}
+          onClick={e => sendMessage(e)}
+          variant="contained" 
+          color="primary"> SEND </Button>
+        </FormControl>
+      {
+        messages.map((message, index) => <Message key={index} message={message} user={username} />)
+      }
     </div>
   );
 }
